06fb8cc1ec48e56e91e99395cba4c773e43f2a7d
Claudia.REIFF <claudia.reiff@yoshie.io>
2023-04-13 11:15:30 +0200

    NSLookup minimum

--- NSLookup/src/main/java/org/example/Main.java
+++ NSLookup/src/main/java/org/example/Main.java
@@ -5,6 +5,10 @@ import java.net.UnknownHostException;
 public class Main {
     public static void main(String[] args) throws UnknownHostException {
         NSLookup nsLookup = new NSLookup();
-        nsLookup.myLookup("52.216.145.50");
+
+        nsLookup.myLookup("www.google.com");
+        nsLookup.myLookup("www.java2s.com");
+        nsLookup.myLookup("8.8.8.8");
+        nsLookup.myLookup("invalid-hostname.com");
     }
 }
--- NSLookup/src/main/java/org/example/NSLookup.java
+++ NSLookup/src/main/java/org/example/NSLookup.java
@@ -7,20 +7,20 @@ import java.net.UnknownHostException;
 public class NSLookup {
     public NSLookup() {}
 
-    public void myLookup(String host) throws UnknownHostException {
+    public void myLookup(String host){
         try {
             if(Inet4Address.getByName(host).getHostAddress().equals(host)){
-                System.out.println(InetAddress.getByName(host));
+                System.out.println(host + " : " + InetAddress.getByName(host).getHostName());
             }
             else{
                 InetAddress[] inetAddresses = InetAddress.getAllByName(host);
                 for (InetAddress inetAddress : inetAddresses) {
-                    System.out.println(inetAddress);
+                    System.out.println(host + " : " + inetAddress.getHostAddress());
                 }
             }
 
         } catch (UnknownHostException e) {
-            System.out.println(e);
+            System.out.println("Host could not be found.");
         }
     }
 }
